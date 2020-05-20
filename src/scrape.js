e9c2690541d61b02d4331d942a749ea228d6128d
Ivan Feofilaktov <perl6@bk.ru>
2020-05-20 18:24:30 +0300

    reject Promise in case of error from axios (i.g. 404 account is not found)

--- src/scrape.js
+++ src/scrape.js
@@ -7,53 +7,58 @@ const { normalizeUrl } = require("../src/util");
  * @return { Promise<Object> }
  */
 module.exports = (username) => {
-  return new Promise(async (resolve) => {
+  return new Promise(async (resolve,reject) => {
     let link = normalizeUrl(username);
-    const GQL = await axios.get(link);
-    let user = GQL.data.graphql.user;
-    resolve({
-      link: link.replace("/?__a=1", ""),
-      id: user.id,
-      biography: user.biography,
-      subscribersCount: user.edge_followed_by.count,
-      subscribtions: user.edge_follow.count,
-      fullName: user.full_name,
-      highlightCount: user.highlight_reel_count,
-      isBusinessAccount: user.is_business_account,
-      isRecentUser: user.is_joined_recently,
-      accountCategory: user.business_category_name,
-      isPrivate: user.is_private,
-      isVerified: user.is_verified,
-      profilePic: user.profile_pic_url,
-      profilePicHD: user.profile_pic_url_hd,
-      username: user.username,
-      postsCount: user.edge_owner_to_timeline_media.count,
-      posts: user.edge_owner_to_timeline_media.edges.map(edge => {
-        let hasCaption = edge.node.edge_media_to_caption.edges[0];
-        return {
-          id: edge.node.id,
-          shortCode: edge.node.shortcode,
-          url: `https://www.instagram.com/p/${edge.node.shortcode}/`,
-          dimensions: edge.node.dimensions,
-          imageUrl: edge.node.display_url,
-          isVideo: edge.node.is_video,
-          caption: hasCaption ? hasCaption.node.text : "",
-          commentsCount: edge.node.edge_media_to_comment.count,
-          commentsDisabled: edge.node.comments_disabled,
-          timestamp: edge.node.taken_at_timestamp,
-          likesCount: edge.node.edge_liked_by.count,
-          location: edge.node.location,
-          childs: edge.node.edge_sidecar_to_children ? edge.node.edge_sidecar_to_children.edges.map(edge => {
-            return {
-              id: edge.node.id,
-              shortCode: edge.node.shortcode,
-              dimensions: edge.node.dimensions,
-              imageUrl: edge.node.display_url,
-              isVideo: edge.node.is_video,
-            }
-          }) : []
-        }
-      }) || []
-    });
+    const GQL = await axios.get(link)
+        .catch(error => {
+          reject(error);
+        });
+    if(GQL) {
+        let user = GQL.data.graphql.user;
+        resolve({
+            link: link.replace("/?__a=1", ""),
+            id: user.id,
+            biography: user.biography,
+            subscribersCount: user.edge_followed_by.count,
+            subscribtions: user.edge_follow.count,
+            fullName: user.full_name,
+            highlightCount: user.highlight_reel_count,
+            isBusinessAccount: user.is_business_account,
+            isRecentUser: user.is_joined_recently,
+            accountCategory: user.business_category_name,
+            isPrivate: user.is_private,
+            isVerified: user.is_verified,
+            profilePic: user.profile_pic_url,
+            profilePicHD: user.profile_pic_url_hd,
+            username: user.username,
+            postsCount: user.edge_owner_to_timeline_media.count,
+            posts: user.edge_owner_to_timeline_media.edges.map(edge => {
+                let hasCaption = edge.node.edge_media_to_caption.edges[0];
+                return {
+                    id: edge.node.id,
+                    shortCode: edge.node.shortcode,
+                    url: `https://www.instagram.com/p/${edge.node.shortcode}/`,
+                    dimensions: edge.node.dimensions,
+                    imageUrl: edge.node.display_url,
+                    isVideo: edge.node.is_video,
+                    caption: hasCaption ? hasCaption.node.text : "",
+                    commentsCount: edge.node.edge_media_to_comment.count,
+                    commentsDisabled: edge.node.comments_disabled,
+                    timestamp: edge.node.taken_at_timestamp,
+                    likesCount: edge.node.edge_liked_by.count,
+                    location: edge.node.location,
+                    childs: edge.node.edge_sidecar_to_children ? edge.node.edge_sidecar_to_children.edges.map(edge => {
+                        return {
+                            id: edge.node.id,
+                            shortCode: edge.node.shortcode,
+                            dimensions: edge.node.dimensions,
+                            imageUrl: edge.node.display_url,
+                            isVideo: edge.node.is_video,
+                        }
+                    }) : []
+                }
+            }) || []
+        });
+    }
   });
 };
