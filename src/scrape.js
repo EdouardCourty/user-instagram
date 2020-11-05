51a59e50c4cff17f1a09af53b375466deb101a46
Edouard Courty <edouard.courty2@gmail.com>
2020-11-05 23:24:14 +0100

    fix (request) fix cors cache control rejection

--- src/scrape.js
+++ src/scrape.js
@@ -13,8 +13,7 @@ module.exports.getUserData = (username) => {
         url: normalizeUrl(username),
         headers: {
             'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
-            'authority': 'www.instagram.com',
-            'cache-control': 'max-age=0'
+            'authority': 'www.instagram.com'
         }
     };
     const GQL = await axios(REQUEST_PARAMETERS)
