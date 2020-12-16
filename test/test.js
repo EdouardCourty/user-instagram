85c88acd7e660df10229478bd166364ed8a95457
Edouard Courty <edouard.courty2@gmail.com>
2020-12-16 11:06:18 +0100

    fix (project) implement unit tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,24 @@
-require("../index").getUserData("edouard_courty")
-  .then(console.log)
-  .catch(console.error);
+const {assert, should, expect} = require('chai');
+const Instagram = require("../index");
 
-require("../index").getPostData("CHN1U3TF3VH")
-  .then(console.log)
-  .catch(console.error);
+describe('Instagram', () => {
+  describe('#User', () => {
+    it('Should return an error if a wrong username is fetched.', async () => {
+      expect(await Instagram.getUserData('ThisIsObviouslyAFakeUsernameItCantExist')).to.throw
+    })
+
+    it('Should return user data', async () => {
+      expect(await Instagram.getUserData('edouardcourty')).not.to.throw
+    })
+  })
+
+  describe("#Post", () => {
+    it('Should return an error if a wrong shortcode is fetched.', async () => {
+      expect(await Instagram.getPostData('ThisShortCodeDoesNotExist')).to.throw
+    })
+
+    it('Should return post data', async () => {
+      expect(await Instagram.getPostData('CHN1U3TF3VH')).not.to.throw
+    })
+  })
+})
